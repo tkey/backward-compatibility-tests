db4a14d0a8631ed0d29108a9759034f72e15d27e
rathishubham7 <shubhamrathi@icloud.com>
2021-04-15 13:10:55 +0530

    added jsdom

--- test/setup.js
+++ test/setup.js
@@ -1,7 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 const path = require("path");
-
+require("jsdom-global")("<!doctype html><html><body></body></html>", {
+  url: "https://example.com",
+});
 require("ts-node").register({ project: path.resolve("tsconfig.json"), require: ["tsconfig-paths/register"] });
 
 const register = require("@babel/register").default;
